081c591772cfa42c149b93c0910b60bdf32c701c
camot0328 <camot0328@gmail.com>
2025-06-23 14:09:59 +0900

    main controller fix

--- src/main/java/org/example/cicd_test/controller/MainController.java
+++ src/main/java/org/example/cicd_test/controller/MainController.java
@@ -8,6 +8,6 @@ public class MainController {
 
   @GetMapping("/")
   public String hello() {
-    return "hello";
+    return "This is a test to gggggggggggggggggggggggggggggggggggggggggggg";
   }
 }
